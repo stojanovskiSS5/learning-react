a49a5081c71b3da032aeaa31e5ab6558ec2ff3ec
stojanovskiSS5 <simon.stoj1995@gmail.com>
2024-11-21 22:02:47 +0100

    add timer and modal features to this game

--- 01-refs/src/components/Player.jsx
+++ 01-refs/src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+
+  const playerNameRef = useRef();
+
+
+  const [enteredName, setEnteredName] = useState(null);
+
+  function handleClick(){
+    setEnteredName(playerNameRef.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredName ?? 'uknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameRef} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
